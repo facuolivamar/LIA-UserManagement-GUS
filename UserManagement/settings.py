acf332309160c2268cc558e32c9250935ef113bf
facuolivamar <facundoolivam@gmail.com>
2025-04-19 22:23:03 -0300

    FEAT: Agregar configuraciones para CSRF y CORS en settings.py

--- UserManagement/settings.py
+++ UserManagement/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CORS_ALLOWED_ORIGINS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
